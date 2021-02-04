d8a16412cabb5d80000ed9853dce59d6bb316253
KooBH <ninie4409@sogang.ac.kr>
2021-02-04 20:58:38 +0900

    cmake for being used as submodule

--- KSpectrogram.cmake
+++ KSpectrogram.cmake
@@ -1,29 +1,24 @@
 ################################################################
 # CMake file for using KSpectrogram as submodule               #
 # 															   #
-# Note : 'config.json' file must exist in '../config.json'     #
 ################################################################
 
 list(APPEND SRC_Qt_MOC
-	${CMAKE_CURRENT_LIST_DIR}/src/KRecordPlot.h
-	${CMAKE_CURRENT_LIST_DIR}/src/KRecorderControl.h
+	${CMAKE_CURRENT_LIST_DIR}/src/KSpectrogram.h
+	${CMAKE_CURRENT_LIST_DIR}/src/KAnalysis.h
 )
 
 list(APPEND SRC_Qt
-	${CMAKE_CURRENT_LIST_DIR}/src/KInput.h
-	${CMAKE_CURRENT_LIST_DIR}/src/KParam.h
-	${CMAKE_CURRENT_LIST_DIR}/src/KRecorder.h
-	${CMAKE_CURRENT_LIST_DIR}/src/KRecorderControl.cpp
-	${CMAKE_CURRENT_LIST_DIR}/src/KRecordPlot.cpp
-	
+	${CMAKE_CURRENT_LIST_DIR}/src/KSpecWidget.h
+	${CMAKE_CURRENT_LIST_DIR}/src/KSpectrogram.cpp
+	${CMAKE_CURRENT_LIST_DIR}/src/KAnalysis.cpp
+	${CMAKE_CURRENT_LIST_DIR}/src/KSpecWidget.cpp
+	${CMAKE_CURRENT_LIST_DIR}/src/BorderLayout.h
+	${CMAKE_CURRENT_LIST_DIR}/src/BorderLayout.cpp
 )
 
 list(APPEND SRC_Qt
-	${CMAKE_CURRENT_LIST_DIR}/src/JsonConfig.h
-	${CMAKE_CURRENT_LIST_DIR}/src/Config.h
+	${CMAKE_CURRENT_LIST_DIR}/src/LogSpec.h
+	${CMAKE_CURRENT_LIST_DIR}/src/ColorMap.h
 	)
 	
-## PATH
-list(APPEND COMPILE_OPTION
-  -D_CONFIG_JSON="../config.json"
-)
